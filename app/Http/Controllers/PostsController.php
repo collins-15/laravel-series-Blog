712b402a491ca7b5c067d36ce0fa0742f0a1e3e6
collins-15 <collinsmutuma15@gmail.com>
2023-06-28 13:58:10 +0300

    eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,19 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     //
+
     public function show($slug)
     {
-
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-       
-
         return view('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
 
     }
